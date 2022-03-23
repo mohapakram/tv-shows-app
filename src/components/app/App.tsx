ba45526fe587d2516cbfb9af2084182c756d1bd6
mohapakram <mohapmobile@gmail.com>
2022-03-23 16:57:14 +0200

    setup project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AppContainer } from './styles/App.styles'
+
+const App = () => (
+  <AppContainer>
+    <h1>hello</h1>
+  </AppContainer>
+)
+
+export default App
